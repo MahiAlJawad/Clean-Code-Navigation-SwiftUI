e005ba4a66086f107cbe775d2bf64febe05b4c6c
MahiAlJawad <br.mahialjawad@gmail.com>
2024-02-10 20:53:55 +0600

    Initial Commit

---
+++ Clean Code Navigation-SwiftUI/Clean_Code_Navigation_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clean_Code_Navigation_SwiftUIApp.swift
+//  Clean Code Navigation-SwiftUI
+//
+//  Created by Mahi Al Jawad on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Clean_Code_Navigation_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
